049d72648844f06f043b4c83cf49155976a5dbf2
Serg Chernata <sergchernata@gmail.com>
2019-06-10 16:27:49 -0400

    initialized

---
+++ config.tf
@@ -0,0 +1,12 @@
+variable "access_key" {}
+variable "secret_key" {}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
+}
