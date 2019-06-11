b4ea4366a2435eccb17d852b5a26109204890bdc
Serg Chernata <sergchernata@gmail.com>
2019-06-11 09:33:51 -0400

    added my key to instance

---
+++ key_pairs.tf
@@ -0,0 +1,4 @@
+resource "aws_key_pair" "serg" {
+  key_name   = "Serg"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC6KpRSB6pzfKDvlvUzySt2qC3+Wimtm/4J22/EhPe3m8RfdO16QFdCLZfTKC+cpFc1IPA1QBFVIG6uxVER/18Q6sXPXDR/p+VuvpPRD2xsgkFdd3o5VMMwfQJ+xVaPyNDyzOgSAW6Oh8EPNoN1xvs1yiSC2Mh8wFRyrfQMloks1TJjVvyLYPJQqo4CRVaUbZxEwYSBl8KRCTa+8wpS+Gllb+MiIu85Msk5jbMIGL0tC76wKTAYwwXi9ZzoQWNP5BF71463DG32U0QwXUAKIxQzzEF+0kSPrOmfYp7h+Pbp57K1YdjN+LhsddcmQJimk5qmXZupDDncRicQDhRC4koh serg@Sergs-MacBook-Pro.local"
+}
