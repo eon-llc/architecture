e77a676ba2ac4758a3e8da161362158483e18564
Serg Chernata <sergchernata@gmail.com>
2019-06-11 09:04:40 -0400

    added website instance

--- ec2.tf
+++ ec2.tf
@@ -1,1 +1,9 @@
+resource "aws_instance" "eon" {
+  ami                    = "ami-0c6b1d09930fac512"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.allow_http.id}", "${aws_security_group.allow_https.id}", "${aws_security_group.allow_ssh.id}"]
 
+  tags = {
+    Name = "Website"
+  }
+}
