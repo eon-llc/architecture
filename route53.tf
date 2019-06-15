01d34a3969af4ac5cb60fd5b4b861938ee9ccbd5
Serg Chernata <sergchernata@gmail.com>
2019-06-15 15:06:58 -0400

    point domain at website instance

--- route53.tf
+++ route53.tf
@@ -1,3 +1,11 @@
 resource "aws_route53_zone" "eon" {
   name = "eon.llc"
 }
+
+resource "aws_route53_record" "eon_website" {
+  zone_id = "${aws_route53_zone.eon.zone_id}"
+  name    = "eon.llc"
+  type    = "A"
+  ttl     = 3600
+  records = ["${aws_instance.eon_website.public_ip}"]
+}
