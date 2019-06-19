5e3bc8ee981a398895a42bfa6a3950007c89a439
Serg Chernata <sergchernata@gmail.com>
2019-06-19 19:13:15 -0400

    don't need ec2 at this time

--- ec2.tf
+++ ec2.tf
@@ -1,13 +1,1 @@
-resource "aws_instance" "eon_website" {
-  ami           = "${var.amazon_linux_ami}"
-  instance_type = "t2.micro"
 
-  subnet_id                   = "${aws_subnet.public_subnet.id}"
-  vpc_security_group_ids      = ["${aws_security_group.allow_web.id}"]
-  associate_public_ip_address = true
-  source_dest_check           = false
-
-  tags = {
-    Name = "Eon Website"
-  }
-}
