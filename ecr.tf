a68604ffe8b15ffb42a2973c9f01944ad7561a4a
Serg Chernata <sergchernata@gmail.com>
2019-06-19 20:00:13 -0400

    added repo for test image

--- ecr.tf
+++ ecr.tf
@@ -46,3 +46,52 @@ resource "aws_ecr_lifecycle_policy" "eon_website_build_policy" {
 }
 EOF
 }
+
+resource "aws_ecr_repository" "eon_website_test" {
+  name = "eon/website_test"
+}
+
+resource "aws_ecr_repository_policy" "eon_website_test_policy" {
+  repository = "${aws_ecr_repository.eon_website_test.name}"
+
+  policy = <<EOF
+{
+    "Version": "2008-10-17",
+    "Statement": [
+        {
+            "Sid": "CodeBuildAccess",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "*"
+            },
+            "Action": [
+                "ecr:*"
+            ]
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_ecr_lifecycle_policy" "eon_website_test_policy" {
+  repository = "${aws_ecr_repository.eon_website_test.name}"
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Keep last 2 images",
+            "selection": {
+                "tagStatus": "untagged",
+                "countType": "imageCountMoreThan",
+                "countNumber": 2
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
