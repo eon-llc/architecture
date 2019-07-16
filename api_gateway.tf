5c0378c3a7813e0537acf4a449f15ad248b75d4d
Serg Chernata <sergchernata@gmail.com>
2019-07-15 20:47:38 -0400

    added CORS

--- api_gateway.tf
+++ api_gateway.tf
@@ -13,6 +13,65 @@ resource "aws_api_gateway_resource" "github" {
   path_part   = "github"
 }
 
+# --------------------------------------------------
+# OPTIONS method, required by AWS to enable CORS
+# https://docs.aws.amazon.com/apigateway/latest/developerguide/how-to-cors.html
+# --------------------------------------------------
+resource "aws_api_gateway_method" "options_method" {
+  rest_api_id   = "${aws_api_gateway_rest_api.eon.id}"
+  resource_id   = "${aws_api_gateway_resource.github.id}"
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method_response" "options" {
+  rest_api_id = "${aws_api_gateway_rest_api.eon.id}"
+  resource_id = "${aws_api_gateway_resource.github.id}"
+  http_method = "${aws_api_gateway_method.options_method.http_method}"
+  depends_on  = ["aws_api_gateway_method.options_method"]
+  status_code = "200"
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
+    "method.response.header.Access-Control-Max-Age"       = true
+  }
+}
+
+resource "aws_api_gateway_integration" "options_integration" {
+  rest_api_id = "${aws_api_gateway_rest_api.eon.id}"
+  resource_id = "${aws_api_gateway_resource.github.id}"
+  http_method = "${aws_api_gateway_method.options_method.http_method}"
+  type        = "MOCK"
+  depends_on  = ["aws_api_gateway_method.options_method"]
+
+  request_templates {
+    "application/json" = "{ \"statusCode\": 200 }"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "options_integration_response" {
+  rest_api_id = "${aws_api_gateway_rest_api.eon.id}"
+  resource_id = "${aws_api_gateway_resource.github.id}"
+  http_method = "${aws_api_gateway_method.options_method.http_method}"
+  status_code = "${aws_api_gateway_method_response.options.status_code}"
+  depends_on  = ["aws_api_gateway_method_response.options"]
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+}
+
+# --------------------------------------------------
+# GET method, for fetching the stats
+# --------------------------------------------------
 resource "aws_api_gateway_method" "github" {
   rest_api_id   = "${aws_api_gateway_rest_api.eon.id}"
   resource_id   = "${aws_api_gateway_resource.github.id}"
@@ -20,34 +79,52 @@ resource "aws_api_gateway_method" "github" {
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "lambda" {
-  rest_api_id = "${aws_api_gateway_rest_api.eon.id}"
-  resource_id = "${aws_api_gateway_method.github.resource_id}"
-  http_method = "${aws_api_gateway_method.github.http_method}"
-
-  integration_http_method = "POST"
-
-  type = "AWS"
-  uri  = "${aws_lambda_function.github_stats.invoke_arn}"
-}
-
 resource "aws_api_gateway_method_response" "github" {
   rest_api_id = "${aws_api_gateway_rest_api.eon.id}"
   resource_id = "${aws_api_gateway_resource.github.id}"
   http_method = "${aws_api_gateway_method.github.http_method}"
   status_code = "200"
+  depends_on  = ["aws_api_gateway_method.github"]
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin"  = true
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+  }
+}
+
+resource "aws_api_gateway_integration" "lambda" {
+  rest_api_id = "${aws_api_gateway_rest_api.eon.id}"
+  resource_id = "${aws_api_gateway_method.github.resource_id}"
+  http_method = "${aws_api_gateway_method.github.http_method}"
+  depends_on  = ["aws_api_gateway_method.github", "aws_lambda_function.github_stats"]
+
+  integration_http_method = "POST"
+
+  type = "AWS"
+  uri  = "${aws_lambda_function.github_stats.invoke_arn}"
+
+  request_templates {
+    "application/json" = "{ \"statusCode\": 200 }"
+  }
 }
 
 resource "aws_api_gateway_integration_response" "lambda" {
   rest_api_id = "${aws_api_gateway_rest_api.eon.id}"
   resource_id = "${aws_api_gateway_resource.github.id}"
   http_method = "${aws_api_gateway_method.github.http_method}"
-
+  depends_on  = ["aws_api_gateway_integration.lambda"]
   status_code = "${aws_api_gateway_method_response.github.status_code}"
 
-  depends_on = [
-    "aws_api_gateway_integration.lambda",
-  ]
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
 }
 
 resource "aws_api_gateway_stage" "github" {
