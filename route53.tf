2ed0c6fce66755ed0e7fe5b743d7a36535b47452
Serg Chernata <sergchernata@gmail.com>
2019-07-19 08:30:15 -0400

    added gmail mx

--- route53.tf
+++ route53.tf
@@ -30,3 +30,11 @@ resource "aws_route53_record" "github_verification" {
 
   records = ["b395afeb96"]
 }
+
+resource "aws_route53_record" "gmail_mx" {
+  name    = "goact.app"
+  type    = "MX"
+  ttl     = 3600
+  records = ["1 ASPMX.L.GOOGLE.COM.", "5 ALT2.ASPMX.L.GOOGLE.COM.", "5 ALT1.ASPMX.L.GOOGLE.COM.", "10 ALT3.ASPMX.L.GOOGLE.COM.", "10 ALT4.ASPMX.L.GOOGLE.COM."]
+  zone_id = "${aws_route53_zone.eon.zone_id}"
+}
