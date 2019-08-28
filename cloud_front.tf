31619617882cb540771010ed137b62b979dded63
Serg Chernata <sergchernata@gmail.com>
2019-08-28 07:51:09 -0400

    added cloudfront distribution for rem api

--- cloud_front.tf
+++ cloud_front.tf
@@ -14,7 +14,7 @@ resource "aws_cloudfront_distribution" "production-bucket-eon" {
   aliases             = ["eon.llc"]
   enabled             = true
   is_ipv6_enabled     = false
-  comment             = "Production distribution"
+  comment             = "Eon website"
   default_root_object = "index.html"
   price_class         = "PriceClass_100"
 
@@ -42,6 +42,60 @@ resource "aws_cloudfront_distribution" "production-bucket-eon" {
     error_code            = 404
     response_page_path    = "/index.html"
     response_code         = 200
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = "${aws_acm_certificate.eon_website.arn}"
+    ssl_support_method  = "sni-only"
+  }
+
+  tags {
+    Environment = "production"
+  }
+}
+
+resource "aws_cloudfront_distribution" "rem_full_node_api" {
+  origin {
+    domain_name = "${aws_instance.rem_full_node.public_dns}"
+    origin_id   = "${aws_instance.rem_full_node.id}"
+
+    custom_origin_config {
+      origin_protocol_policy = "match-viewer"
+      http_port              = "80"
+      https_port             = "443"
+      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
+  }
+
+  aliases         = ["rem.eon.llc"]
+  enabled         = true
+  is_ipv6_enabled = false
+  comment         = "REM API"
+  price_class     = "PriceClass_100"
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["HEAD", "GET", "OPTIONS"]
+    target_origin_id = "${aws_instance.rem_full_node.id}"
+
+    forwarded_values {
+      query_string = true
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 0
+    max_ttl                = 0
   }
 
   restrictions {
