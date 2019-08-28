08e2eebb403b934457b00ba1950144c97d3392ba
Serg Chernata <sergchernata@gmail.com>
2019-08-28 07:51:36 -0400

    added full node instance and elastic ips for both nodes

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "test_bp" {
+resource "aws_instance" "rem_producing_node" {
   ami           = "${var.ubuntu_18_ami}"
   instance_type = "r5.large"
   key_name      = "${aws_key_pair.serg.key_name}"
@@ -8,26 +8,73 @@ resource "aws_instance" "test_bp" {
   associate_public_ip_address = true
   source_dest_check           = false
 
-  user_data = "${data.template_file.test_node_init.rendered}"
+  user_data = "${data.template_file.rem_producing_node_init.rendered}"
 
   tags = {
-    Name = "REM Testnet BP"
+    Name = "REM Producing Node"
+  }
+
+  volume_tags = {
+    Name = "REM Producing Node OS"
   }
 }
 
-resource "aws_eip" "test_bp_ip" {
-  instance   = "${aws_instance.test_bp.id}"
+resource "aws_eip" "rem_producing_node_ip" {
+  instance   = "${aws_instance.rem_producing_node.id}"
   depends_on = ["aws_internet_gateway.gw"]
   vpc        = true
 }
 
-data "template_file" "test_node_init" {
-  template = "${file("user_data/testnet_node.sh.tpl")}"
+data "template_file" "rem_producing_node_init" {
+  template = "${file("user_data/rem_producing_node.sh.tpl")}"
 
   vars {
-    domain       = "${var.eon_domain}"
-    account_name = "${var.testnet_account_name}"
-    public_key   = "${var.testnet_public_key}"
-    private_key  = "${var.testnet_private_key}"
+    domain           = "${var.eon_domain}"
+    account_name     = "${var.testnet_account_name}"
+    public_key       = "${var.testnet_public_key}"
+    private_key      = "${var.testnet_private_key}"
+    rem_peer_address = "${var.rem_peer_address}"
+  }
+}
+
+resource "aws_instance" "rem_full_node" {
+  ami           = "${var.ubuntu_18_ami}"
+  instance_type = "t2.xlarge"
+  key_name      = "${aws_key_pair.serg.key_name}"
+
+  vpc_security_group_ids      = ["${aws_security_group.allow_ssh.id}", "${aws_security_group.allow_web.id}", "${aws_security_group.rem_core.id}"]
+  subnet_id                   = "${aws_subnet.public_subnet.id}"
+  associate_public_ip_address = true
+  source_dest_check           = false
+
+  user_data = "${data.template_file.rem_full_node_init.rendered}"
+
+  root_block_device {
+    volume_size = 20
+  }
+
+  tags = {
+    Name = "REM Full Node"
+  }
+
+  volume_tags = {
+    Name = "REM Full Node OS"
+  }
+}
+
+resource "aws_eip" "rem_full_node" {
+  instance   = "${aws_instance.rem_full_node.id}"
+  depends_on = ["aws_internet_gateway.gw"]
+  vpc        = true
+}
+
+data "template_file" "rem_full_node_init" {
+  template = "${file("user_data/rem_full_node.sh.tpl")}"
+
+  vars {
+    postgres_db      = "${var.postgres_db}"
+    postgres_user    = "${var.postgres_user}"
+    postgres_pass    = "${var.postgres_pass}"
+    rem_peer_address = "${var.rem_peer_address}"
   }
 }
