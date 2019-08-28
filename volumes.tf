e371ccbaf0ef751822bed11f34358dcb730747dd
Serg Chernata <sergchernata@gmail.com>
2019-08-28 07:53:10 -0400

    added postgres volume

--- volumes.tf
+++ volumes.tf
@@ -1,15 +1,31 @@
-resource "aws_ebs_volume" "test_bp" {
+resource "aws_ebs_volume" "rem_producing_node" {
   availability_zone = "${var.region}a"
   size              = 30
   type              = "gp2"
 
   tags {
-    Name = "REM Testnet BP"
+    Name = "REM Producing Node"
   }
 }
 
-resource "aws_volume_attachment" "test_bp_ebs_att" {
+resource "aws_volume_attachment" "rem_producing_node_ebs_att" {
   device_name = "/dev/sdf"
-  volume_id   = "${aws_ebs_volume.test_bp.id}"
-  instance_id = "${aws_instance.test_bp.id}"
+  volume_id   = "${aws_ebs_volume.rem_producing_node.id}"
+  instance_id = "${aws_instance.rem_producing_node.id}"
+}
+
+resource "aws_ebs_volume" "rem_postgesql" {
+  availability_zone = "${var.region}a"
+  size              = 100
+  type              = "gp2"
+
+  tags {
+    Name = "REM PostgreSQL"
+  }
+}
+
+resource "aws_volume_attachment" "rem_postgesql_ebs_att" {
+  device_name = "/dev/sdf"
+  volume_id   = "${aws_ebs_volume.rem_postgesql.id}"
+  instance_id = "${aws_instance.rem_full_node.id}"
 }
