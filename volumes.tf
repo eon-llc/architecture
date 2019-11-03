7bf4603abdfff9aa75338540a24870c5de66c585
Serg Chernata <sergchernata@gmail.com>
2019-11-03 16:58:48 -0500

    added volume for storage of benchmark results

--- volumes.tf
+++ volumes.tf
@@ -29,3 +29,19 @@ resource "aws_volume_attachment" "rem_rocksdb_ebs_att" {
   volume_id   = "${aws_ebs_volume.rem_rocksdb.id}"
   instance_id = "${aws_instance.rem_full_node.id}"
 }
+
+resource "aws_ebs_volume" "rem_benchmark" {
+  availability_zone = "${var.region}a"
+  size              = 10
+  type              = "gp2"
+
+  tags {
+    Name = "REM Benchmark"
+  }
+}
+
+resource "aws_volume_attachment" "rem_benchmark_ebs_att" {
+  device_name = "/dev/sdf"
+  volume_id   = "${aws_ebs_volume.rem_benchmark.id}"
+  instance_id = "${aws_instance.rem_benchmark.id}"
+}
