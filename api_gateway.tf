3d73d3f1ade8cf735520b21339b52daf3d38ba39
Serg Chernata <sergchernata@gmail.com>
2019-11-13 15:39:09 -0500

    refactord naming convention, added blog endpoint

--- api_gateway.tf
+++ api_gateway.tf
@@ -13,22 +13,29 @@ resource "aws_api_gateway_resource" "github" {
   path_part   = "github"
 }
 
+resource "aws_api_gateway_resource" "blog" {
+  rest_api_id = "${aws_api_gateway_rest_api.eon.id}"
+  parent_id   = "${aws_api_gateway_rest_api.eon.root_resource_id}"
+  path_part   = "blog"
+}
+
 # --------------------------------------------------
-# OPTIONS method, required by AWS to enable CORS
+# OPTIONS method, for fetching GitHub stats
+# required by AWS to enable CORS
 # https://docs.aws.amazon.com/apigateway/latest/developerguide/how-to-cors.html
 # --------------------------------------------------
-resource "aws_api_gateway_method" "options_method" {
+resource "aws_api_gateway_method" "github_options" {
   rest_api_id   = "${aws_api_gateway_rest_api.eon.id}"
   resource_id   = "${aws_api_gateway_resource.github.id}"
   http_method   = "OPTIONS"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_method_response" "options" {
+resource "aws_api_gateway_method_response" "github_options" {
   rest_api_id = "${aws_api_gateway_rest_api.eon.id}"
   resource_id = "${aws_api_gateway_resource.github.id}"
-  http_method = "${aws_api_gateway_method.options_method.http_method}"
-  depends_on  = ["aws_api_gateway_method.options_method"]
+  http_method = "${aws_api_gateway_method.github_options.http_method}"
+  depends_on  = ["aws_api_gateway_method.github_options"]
   status_code = "200"
 
   response_models = {
@@ -43,24 +50,24 @@ resource "aws_api_gateway_method_response" "options" {
   }
 }
 
-resource "aws_api_gateway_integration" "options_integration" {
+resource "aws_api_gateway_integration" "github_options" {
   rest_api_id = "${aws_api_gateway_rest_api.eon.id}"
   resource_id = "${aws_api_gateway_resource.github.id}"
-  http_method = "${aws_api_gateway_method.options_method.http_method}"
+  http_method = "${aws_api_gateway_method.github_options.http_method}"
   type        = "MOCK"
-  depends_on  = ["aws_api_gateway_method.options_method"]
+  depends_on  = ["aws_api_gateway_method.github_options"]
 
   request_templates {
     "application/json" = "{ \"statusCode\": 200 }"
   }
 }
 
-resource "aws_api_gateway_integration_response" "options_integration_response" {
+resource "aws_api_gateway_integration_response" "github_options" {
   rest_api_id = "${aws_api_gateway_rest_api.eon.id}"
   resource_id = "${aws_api_gateway_resource.github.id}"
-  http_method = "${aws_api_gateway_method.options_method.http_method}"
-  status_code = "${aws_api_gateway_method_response.options.status_code}"
-  depends_on  = ["aws_api_gateway_method_response.options"]
+  http_method = "${aws_api_gateway_method.github_options.http_method}"
+  status_code = "${aws_api_gateway_method_response.github_options.status_code}"
+  depends_on  = ["aws_api_gateway_method_response.github_options"]
 
   response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
@@ -72,19 +79,19 @@ resource "aws_api_gateway_integration_response" "options_integration_response" {
 # --------------------------------------------------
 # GET method, for fetching GitHub stats
 # --------------------------------------------------
-resource "aws_api_gateway_method" "github" {
+resource "aws_api_gateway_method" "github_get" {
   rest_api_id   = "${aws_api_gateway_rest_api.eon.id}"
   resource_id   = "${aws_api_gateway_resource.github.id}"
   http_method   = "GET"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_method_response" "github" {
+resource "aws_api_gateway_method_response" "github_get" {
   rest_api_id = "${aws_api_gateway_rest_api.eon.id}"
   resource_id = "${aws_api_gateway_resource.github.id}"
-  http_method = "${aws_api_gateway_method.github.http_method}"
+  http_method = "${aws_api_gateway_method.github_get.http_method}"
   status_code = "200"
-  depends_on  = ["aws_api_gateway_method.github"]
+  depends_on  = ["aws_api_gateway_method.github_get"]
 
   response_models = {
     "application/json" = "Empty"
@@ -97,11 +104,11 @@ resource "aws_api_gateway_method_response" "github" {
   }
 }
 
-resource "aws_api_gateway_integration" "lambda" {
+resource "aws_api_gateway_integration" "github_lambda" {
   rest_api_id = "${aws_api_gateway_rest_api.eon.id}"
-  resource_id = "${aws_api_gateway_method.github.resource_id}"
-  http_method = "${aws_api_gateway_method.github.http_method}"
-  depends_on  = ["aws_api_gateway_method.github", "aws_lambda_function.github_stats"]
+  resource_id = "${aws_api_gateway_method.github_get.resource_id}"
+  http_method = "${aws_api_gateway_method.github_get.http_method}"
+  depends_on  = ["aws_api_gateway_method.github_get", "aws_lambda_function.github_stats"]
 
   integration_http_method = "POST"
 
@@ -113,12 +120,12 @@ resource "aws_api_gateway_integration" "lambda" {
   }
 }
 
-resource "aws_api_gateway_integration_response" "lambda" {
+resource "aws_api_gateway_integration_response" "github_lambda" {
   rest_api_id = "${aws_api_gateway_rest_api.eon.id}"
   resource_id = "${aws_api_gateway_resource.github.id}"
-  http_method = "${aws_api_gateway_method.github.http_method}"
-  depends_on  = ["aws_api_gateway_integration.lambda"]
-  status_code = "${aws_api_gateway_method_response.github.status_code}"
+  http_method = "${aws_api_gateway_method.github_get.http_method}"
+  depends_on  = ["aws_api_gateway_integration.github_lambda"]
+  status_code = "${aws_api_gateway_method_response.github_get.status_code}"
 
   response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
@@ -127,7 +134,125 @@ resource "aws_api_gateway_integration_response" "lambda" {
   }
 }
 
-resource "aws_api_gateway_stage" "github" {
+# --------------------------------------------------
+# OPTIONS method, for fetching Blog stats
+# required by AWS to enable CORS
+# https://docs.aws.amazon.com/apigateway/latest/developerguide/how-to-cors.html
+# --------------------------------------------------
+resource "aws_api_gateway_method" "blog_options" {
+  rest_api_id   = "${aws_api_gateway_rest_api.eon.id}"
+  resource_id   = "${aws_api_gateway_resource.blog.id}"
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method_response" "blog_options" {
+  rest_api_id = "${aws_api_gateway_rest_api.eon.id}"
+  resource_id = "${aws_api_gateway_resource.blog.id}"
+  http_method = "${aws_api_gateway_method.blog_options.http_method}"
+  depends_on  = ["aws_api_gateway_method.blog_options"]
+  status_code = "200"
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
+    "method.response.header.Access-Control-Max-Age"       = true
+  }
+}
+
+resource "aws_api_gateway_integration" "blog_options" {
+  rest_api_id = "${aws_api_gateway_rest_api.eon.id}"
+  resource_id = "${aws_api_gateway_resource.blog.id}"
+  http_method = "${aws_api_gateway_method.blog_options.http_method}"
+  type        = "MOCK"
+  depends_on  = ["aws_api_gateway_method.blog_options"]
+
+  request_templates {
+    "application/json" = "{ \"statusCode\": 200 }"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "blog_options" {
+  rest_api_id = "${aws_api_gateway_rest_api.eon.id}"
+  resource_id = "${aws_api_gateway_resource.blog.id}"
+  http_method = "${aws_api_gateway_method.blog_options.http_method}"
+  status_code = "${aws_api_gateway_method_response.blog_options.status_code}"
+  depends_on  = ["aws_api_gateway_method_response.blog_options"]
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+}
+
+# --------------------------------------------------
+# GET method, for fetching Blog stats
+# --------------------------------------------------
+resource "aws_api_gateway_method" "blog_get" {
+  rest_api_id   = "${aws_api_gateway_rest_api.eon.id}"
+  resource_id   = "${aws_api_gateway_resource.blog.id}"
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method_response" "blog_get" {
+  rest_api_id = "${aws_api_gateway_rest_api.eon.id}"
+  resource_id = "${aws_api_gateway_resource.blog.id}"
+  http_method = "${aws_api_gateway_method.blog_get.http_method}"
+  status_code = "200"
+  depends_on  = ["aws_api_gateway_method.blog_get"]
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin"  = true
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+  }
+}
+
+resource "aws_api_gateway_integration" "blog_lambda" {
+  rest_api_id = "${aws_api_gateway_rest_api.eon.id}"
+  resource_id = "${aws_api_gateway_method.blog_get.resource_id}"
+  http_method = "${aws_api_gateway_method.blog_get.http_method}"
+  depends_on  = ["aws_api_gateway_method.blog_get", "aws_lambda_function.blog_stats"]
+
+  integration_http_method = "POST"
+
+  type = "AWS"
+  uri  = "${aws_lambda_function.blog_stats.invoke_arn}"
+
+  request_templates {
+    "application/json" = "{ \"statusCode\": 200 }"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "blog_lambda" {
+  rest_api_id = "${aws_api_gateway_rest_api.eon.id}"
+  resource_id = "${aws_api_gateway_resource.blog.id}"
+  http_method = "${aws_api_gateway_method.blog_get.http_method}"
+  depends_on  = ["aws_api_gateway_integration.blog_lambda"]
+  status_code = "${aws_api_gateway_method_response.blog_get.status_code}"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+}
+
+# --------------------------------------------------
+# DEPLOYMENT
+# --------------------------------------------------
+resource "aws_api_gateway_stage" "v1" {
   stage_name            = "v1"
   rest_api_id           = "${aws_api_gateway_rest_api.eon.id}"
   deployment_id         = "${aws_api_gateway_deployment.eon.id}"
@@ -135,10 +260,10 @@ resource "aws_api_gateway_stage" "github" {
   cache_cluster_size    = 0.5
 }
 
-resource "aws_api_gateway_method_settings" "github" {
+resource "aws_api_gateway_method_settings" "v1" {
   rest_api_id = "${aws_api_gateway_rest_api.eon.id}"
-  stage_name  = "${aws_api_gateway_stage.github.stage_name}"
-  method_path = "${aws_api_gateway_resource.github.path_part}/${aws_api_gateway_method.github.http_method}"
+  stage_name  = "${aws_api_gateway_stage.v1.stage_name}"
+  method_path = "*/*"
 
   settings {
     caching_enabled      = true
@@ -146,15 +271,14 @@ resource "aws_api_gateway_method_settings" "github" {
   }
 }
 
-# --------------------------------------------------
-# DEPLOYMENT
-# --------------------------------------------------
 resource "aws_api_gateway_deployment" "eon" {
   rest_api_id = "${aws_api_gateway_rest_api.eon.id}"
 
   depends_on = [
-    "aws_api_gateway_integration.lambda",
-    "aws_api_gateway_integration_response.lambda",
+    "aws_api_gateway_integration.github_lambda",
+    "aws_api_gateway_integration.blog_lambda",
+    "aws_api_gateway_integration_response.github_lambda",
+    "aws_api_gateway_integration_response.blog_lambda",
   ]
 
   // forces a re-deployment
